64095db93f9a728e15535b29c67698d0613f4e2e
Sergio Hernández Navarro <sergio.h.navarro@finetwork.com>
2023-03-22 09:55:01 +0100

    implement incremental static generation for name and id

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -114,7 +114,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemonNames.map(name => ({
             params: { name }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -122,10 +122,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
 
+    const pokemon = await getPokemonInfo(name);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
-        }
+            pokemon
+        },
+        revalidate: 86400
     }
 }
 
